3676798af5aee3dbec60f7f124cb8df080b4b888
Tom Deneire <tomdeneire@gmail.com>
2023-10-06 14:02:03 +0200

    add newlines

--- bench.sh
+++ bench.sh
@@ -1,5 +1,5 @@
-echo 'test_urllib.py' && time for x in {1..10}; do py3 test_urllib.py; done
-echo 'test_urllib3.py' && time for x in {1..10}; do py3 test_urllib3.py; done
-echo 'test_requests.py' &&  time for x in {1..10}; do py3 test_requests.py; done
-echo 'test_aiohttp.py' && time for x in {1..10}; do py3 test_aiohttp.py; done
-echo 'test_httpx.py' && time for x in {1..10}; do py3 test_httpx.py; done
+echo -e '\ntest_urllib.py' && time for x in {1..10}; do py3 test_urllib.py; done
+echo -e '\ntest_urllib3.py' && time for x in {1..10}; do py3 test_urllib3.py; done
+echo -e '\ntest_requests.py' &&  time for x in {1..10}; do py3 test_requests.py; done
+echo -e '\ntest_aiohttp.py' && time for x in {1..10}; do py3 test_aiohttp.py; done
+echo -e '\ntest_httpx.py' && time for x in {1..10}; do py3 test_httpx.py; done
