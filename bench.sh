140b0f9dbe0745bd25b6d20c8df0416500720846
Tom Deneire <tomdeneire@gmail.com>
2023-10-06 13:55:27 +0200

    add test name

--- bench.sh
+++ bench.sh
@@ -1,5 +1,5 @@
-time for x in {1..10}; do py3 test_urllib.py; done
-time for x in {1..10}; do py3 test_urllib3.py; done
-time for x in {1..10}; do py3 test_requests.py; done
-time for x in {1..10}; do py3 test_aiohttp.py; done
-time for x in {1..10}; do py3 test_httpx.py; done
+echo 'test_urllib.py' && time for x in {1..10}; do py3 test_urllib.py; done
+echo 'test_urllib3.py' && time for x in {1..10}; do py3 test_urllib3.py; done
+echo 'test_requests.py' &&  time for x in {1..10}; do py3 test_requests.py; done
+echo 'test_aiohttp.py' && time for x in {1..10}; do py3 test_aiohttp.py; done
+echo 'test_httpx.py' && time for x in {1..10}; do py3 test_httpx.py; done
